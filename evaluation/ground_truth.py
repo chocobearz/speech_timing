7ad41932001e2e33182fdc0a11e02b1da9ab812e
chocobearz <ptuttosi@gmail.com>
2022-11-23 23:06:19 -0800

    seperate relaive and absolute ground truth

--- evaluation/ground_truth.py
+++ evaluation/ground_truth.py
@@ -2,24 +2,17 @@ import pandas as pd
 import statistics
 import json
 
-data = pd.read_csv("../data/clean_data.csv",converters={"base_word_lengths": lambda x: x.strip("[]").split(", "), "neutral_relative_word_lengths": lambda x: x.strip("[]").split(", "), "pause_lengths": lambda x: x.strip("[]").split(", ")})
+data = pd.read_csv("../data/clean_data.csv",converters={"base_word_lengths": lambda x: x.strip("[]").split(", ")})
 
-# Sepreate neural data as it has no relative lengths
-data_neutral = data[data["emotion"]  == "N"]
-data_neutral.reset_index(drop=True, inplace=True)
-data_neutral = data_neutral[["base_word_lengths", "script"]]
-
-# non neutral
-data = data[data["emotion"]  != "N"]
-data.reset_index(drop=True, inplace=True)
-data = data[["filename", "neutral_relative_word_lengths", "script", "emotion"]]
+data = data[["filename", "base_word_lengths", "script", "emotion"]]
 
 emotions = [
   "A",
   "D",
   "F",
   "H",
-  "S"
+  "S",
+  "N"
 ]
 
 scripts = [
@@ -38,61 +31,14 @@ scripts = [
 ]
 
 # clean up rows with missing word lengths
-for index, row in data_neutral.iterrows():
-    for i in row["base_word_lengths"]:
-        if i == '':
-            data_neutral.drop(index, inplace=True)
-
-# make list of lengths numbers
-for index, row in data_neutral.iterrows():
-    data_neutral["base_word_lengths"][index] = [float(i) for i in data_neutral["base_word_lengths"][index]]
-
-script0 = data_neutral[data_neutral["script"] == scripts[0]]
-script1 = data_neutral[data_neutral["script"] == scripts[1]]
-script2 = data_neutral[data_neutral["script"] == scripts[2]]
-script3 = data_neutral[data_neutral["script"] == scripts[3]]
-script4 = data_neutral[data_neutral["script"] == scripts[4]]
-script5 = data_neutral[data_neutral["script"] == scripts[5]]
-script6 = data_neutral[data_neutral["script"] == scripts[6]]
-script7 = data_neutral[data_neutral["script"] == scripts[7]]
-script8 = data_neutral[data_neutral["script"] == scripts[8]]
-script9 = data_neutral[data_neutral["script"] == scripts[9]]
-script10 = data_neutral[data_neutral["script"] == scripts[10]]
-script11 = data_neutral[data_neutral["script"] == scripts[11]]
-
-script_dfs = [script0, script1, script2, script3, script4, script5, script6, script7, script8, script9, script10, script11]
-
-average_word_lengths = {}
-
-# get word lengths averaged over all factors for each script
-for script_df in script_dfs:
-  script = script_df['script'].iloc[0]
-  average_word_lengths[script] = []
-  for i in range(len(script_df['base_word_lengths'].iloc[0])):
-    word_lengths = []
-    for index, row in script_df.iterrows():
-      word_lengths.append(row['base_word_lengths'][i])
-    average_word_lengths[script].append(statistics.mean(word_lengths))
-
-print("Writing")
-print(average_word_lengths)
-print("to /data/neutral_avg_word_length.json")
-#save to JSON file
-with open('../data/neutral_avg_word_length.json', 'w') as fp:
-    json.dump(average_word_lengths, fp)
-
-##########################################################################################
-####################NON NEUTRAL DATA######################################################
-
-# clean up rows with missing word lengths
 for index, row in data.iterrows():
-    for i in row["neutral_relative_word_lengths"]:
+    for i in row["base_word_lengths"]:
         if i == '':
             data.drop(index, inplace=True)
 
 # make list of lengths numbers
 for index, row in data.iterrows():
-    data["neutral_relative_word_lengths"][index] = [float(i) for i in data["neutral_relative_word_lengths"][index]]
+    data["base_word_lengths"][index] = [float(i) for i in data["base_word_lengths"][index]]
 
 # add intensity and speaker columns
 data['speaker'] = ""
@@ -111,16 +57,16 @@ for emotion in emotions:
   for script in scripts:
     average_word_lengths[script] = []
     df = data[(data["emotion"] == emotion) & (data["script"] == script)]
-    for i in range(len(df['neutral_relative_word_lengths'].iloc[0])):
+    for i in range(len(df['base_word_lengths'].iloc[0])):
       word_lengths = []
       for index, row in df.iterrows():
-        word_lengths.append(row['neutral_relative_word_lengths'][i])
+        word_lengths.append(row['base_word_lengths'][i])
       average_word_lengths[script].append(statistics.mean(word_lengths))
-  with open(f'../data/{emotion}_avg_word_length.json', 'w') as fp:
+  with open(f'../data/{emotion}_avg_word_length_base.json', 'w') as fp:
     json.dump(average_word_lengths, fp)
   print("Writing")
   print(average_word_lengths)
-  print(f"to /data/{emotion}_avg_word_length.json")
+  print(f"to /data/{emotion}_avg_word_length_base.json")
 
 
 # if working by intensity
